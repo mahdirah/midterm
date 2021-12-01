079291cd0cb29042c828d46f65b2d7e8669a44f3
mahdirah <mahdirah77@gmail.com>
2021-12-02 02:08:16 +0330

    api call and localstorage done

--- script.js
+++ script.js
@@ -5,26 +5,61 @@
             return true
         } else e.preventDefault()
     })
-
     document.querySelector('form').addEventListener('submit', (e) => {
         e.preventDefault()
-
     })
 })()
 
 
-function onSubmit() {
-    const name = document.querySelector('#name').value
-    console.log(name)
+const radios = document.getElementsByName('gender');
 
-    const radios = document.getElementsByName('gender');
-    console.log(radios)
-    
-    let gender
-    for (var i = 0, length = radios.length; i < length; i++) {
+let gender
+
+function getFromStorage(name) {
+    return localStorage.getItem(name)
+}
+
+function onSubmit() {
+    let name = document.querySelector('#name').value
+    const postPromise = fetch("https://api.genderize.io/?name=" + name, {
+        method: 'GET'
+    })
+    postPromise.then((res) => {
+        return res.json()
+    }).then(res => {
+        document.querySelector('#predicted-gender').innerHTML = res.gender
+        document.querySelector('#predicted-percent').innerHTML = res.probability
+
+        if (getFromStorage(name)) {
+            document.querySelector('#saved-gender').innerHTML = getFromStorage(name)
+            document.getElementById('has-save').style.display = "block"
+        }
+        else {
+            document.querySelector('#saved-gender').innerHTML = ""
+            document.getElementById('has-save').style.display = "none"
+        }
+    }).catch(err => {
+        return err
+    })
+}
+
+function onSave() {
+    let name = document.querySelector('#name').value
+    for (let i = 0, length = radios.length; i < length; i++) {
         if (radios[i].checked) {
             gender = radios[i].value
-            console.log(gender)
         }
     }
+    localStorage.setItem(name, gender)
+    document.querySelector('#saved-gender').innerHTML = getFromStorage(name)
+    document.getElementById('has-save').style.display = "block"
+
+}
+
+
+function onRemoveLocal(){
+    let name = document.querySelector('#name').value
+    localStorage.removeItem(name)
+    document.getElementById('has-save').style.display = "none"
+
 }
